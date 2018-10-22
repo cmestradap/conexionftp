1b1d4ecfca9a195c4ec38610fd093f1015c8ad38
cmestradap <carlos1116@gmail.com>
2018-10-22 16:18:27 -0500

    Initial Commit

---
+++ ConexionFTP.java
@@ -0,0 +1,20 @@
+/*Desarrollar la conexión a un servicio FTP
+
+*/
+
+
+import org.apache.commons.net.ftp.FTPClient; //Class that encapsulates all the functionality necessary to store and retrieve files from an FTP server.
+import java.io.IOException;
+
+
+public class ConexionFtp{
+
+    public static void main(String[] args) {
+        FTPClient cliente = new FTPClient(); //Instancia cliente de tipo FTPClient
+
+
+    
+    }
+
+    
+}
---
+++ pruebas.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+
+public class pruebas{
+
+    public static void main(String[] args) {
+        
+    }
+}
